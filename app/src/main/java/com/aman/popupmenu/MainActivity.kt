a47de4a9fe3999673ba187021afa398d431164b1
kauramanp <33446281+kauramanp@users.noreply.github.com>
2024-09-02 01:35:11 +0530

    Pop up menu added

--- app/src/main/java/com/aman/popupmenu/MainActivity.kt
+++ app/src/main/java/com/aman/popupmenu/MainActivity.kt
@@ -3,18 +3,44 @@ package com.aman.popupmenu
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.aman.popupmenu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding: ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    lateinit var popUpMenu : PopupMenu
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnPopUpMenu.setOnClickListener{
+            var popUpMenu = PopupMenu(this@MainActivity, it)
+            popUpMenu.inflate(R.menu.pop_up_menu)
+            popUpMenu.setOnMenuItemClickListener {
+                return@setOnMenuItemClickListener true
+            }
+
+            try{
+                val popUp = PopupMenu::class.java.getDeclaredField("mPopup")
+                popUp.isAccessible = true
+                val menu = popUp.get(popUpMenu)
+                menu.javaClass
+                    .getDeclaredMethod("setForceShowIcon", Boolean::class.java)
+                    .invoke(menu, true)
+            }catch (exception: Exception){} finally {
+                popUpMenu.show()
+
+            }
         }
     }
 }
