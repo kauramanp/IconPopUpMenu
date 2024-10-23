ae70b593fa0ad090546d381945cb9b79a8a3f2a1
kauramanp <33446281+kauramanp@users.noreply.github.com>
2024-10-23 23:49:10 +0530

    Custom pop up window added to show as popup

--- app/src/main/java/com/aman/popupmenu/MainActivity.kt
+++ app/src/main/java/com/aman/popupmenu/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.aman.popupmenu
 
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.PopupWindow
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.PopupMenu
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.aman.popupmenu.databinding.ActivityMainBinding
+import com.aman.popupmenu.databinding.LayoutPopUpWindowBinding
 
 class MainActivity : AppCompatActivity() {
-    val binding: ActivityMainBinding by lazy{
+    val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-    lateinit var popUpMenu : PopupMenu
+    lateinit var popUpMenu: PopupMenu
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,7 +28,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        binding.btnPopUpMenu.setOnClickListener{
+        binding.btnPopUpMenu.setOnClickListener {
             var popUpMenu = PopupMenu(this@MainActivity, it)
             popUpMenu.inflate(R.menu.pop_up_menu)
             popUpMenu.setOnMenuItemClickListener {
@@ -32,22 +37,36 @@ class MainActivity : AppCompatActivity() {
             popUpMenu.show()
         }
 
-        binding.btnCustomLayout.setOnClickListener{
+        binding.btnIconPopUpLayout.setOnClickListener {
             var popUpMenu = PopupMenu(this@MainActivity, it)
             popUpMenu.inflate(R.menu.pop_up_menu)
             popUpMenu.setOnMenuItemClickListener {
                 return@setOnMenuItemClickListener true
             }
 
-            try{
+            try {
                 val popUp = PopupMenu::class.java.getDeclaredField("mPopup")
                 popUp.isAccessible = true
                 val menu = popUp.get(popUpMenu)
                 menu.javaClass
                     .getDeclaredMethod("setForceShowIcon", Boolean::class.java)
                     .invoke(menu, true)
-            }catch (exception: Exception){} finally {
+            } catch (exception: Exception) {
+            } finally {
                 popUpMenu.show()
+            }
+        }
+
+        binding.btnCustomPopUpLayout.setOnClickListener {
+            var popUpBinding = LayoutPopUpWindowBinding.inflate(layoutInflater)
+            var popupWindow = PopupWindow(popUpBinding.root,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                true)
+            popupWindow.showAsDropDown(it)
+            popupWindow.showAtLocation(it, Gravity.CENTER, 0, 0)
+            popUpBinding.llPlay.setOnClickListener {
+                popupWindow.dismiss()
 
             }
         }
